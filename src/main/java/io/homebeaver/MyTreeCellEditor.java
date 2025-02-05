443ed7a3f268195093f8e5ab5f72a70509405220
homebeaver <homebeaver@outlook.de>
2025-02-05 13:09:47 +0100

    MyTreeCellEditor extends DefaultTreeCellEditor

--- src/main/java/io/homebeaver/MyTreeCellEditor.java
+++ src/main/java/io/homebeaver/MyTreeCellEditor.java
@@ -2,13 +2,11 @@ package io.homebeaver;
 
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.Font;
 import java.util.EventObject;
 import java.util.logging.Logger;
 
 import javax.swing.DefaultCellEditor;
-import javax.swing.Icon;
 import javax.swing.JComponent;
 import javax.swing.JTree;
 import javax.swing.SwingUtilities;
@@ -17,6 +15,7 @@ import javax.swing.border.Border;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.tree.DefaultTreeCellEditor;
 import javax.swing.tree.TreeCellEditor;
+import javax.swing.tree.TreePath;
 
 import org.jdesktop.swingx.JXPanel;
 import org.jdesktop.swingx.plaf.UIDependent;
@@ -33,6 +32,34 @@ import io.homebeaver.uom.UoMTreeNode;
  *  TreeCellEditor editor) // realEditor bei null gibt es ein default protected createTreeCellEditor()
  * und protected Container editingContainer = createContainer(); 
  *   + inner public class EditorContainer extends Container
+
+editingContainer wo kommt es vor (in super):
+- in ctor:
+        editingContainer = createContainer();
+- in public Component getTreeCellEditorComponent(JTree tree, Object value, ...
+        if (editingComponent != null) {
+            editingContainer.remove(editingComponent);
+        }
+        editingComponent = realEditor.getTreeCellEditorComponent(tree, value,
+                                        isSelected, expanded,leaf, row);
+... und
+        editingContainer.setFont(font);
+        prepareForEditing();
+        return editingContainer;
+- in protected void prepareForEditing() {
+        if (editingComponent != null) {
+            editingContainer.add(editingComponent);
+        }
+    }
+- in private void cleanupAfterEditing() {
+        if (editingComponent != null) {
+            editingContainer.remove(editingComponent);
+        }
+        editingComponent = null;
+    }
+-- cleanupAfterEditing wird in public stopCellEditing und public cancelCellEditing verwendet
+
+
  * 
  * @author EUGen
  */
@@ -144,8 +171,10 @@ public class MyTreeCellEditor extends DefaultTreeCellEditor implements UIDepende
     // code copied from org.jdesktop.swingx.tree.DefaultXTreeCellEditor
     // used in ctor
     protected Container createContainer() {
-    	LOG.info("-------editingContainer-------");
-        return new XEditorContainer();
+//    	LOG.info("-------editingContainer im Tree Panel-------");
+//        return new XEditorContainer();
+    	LOG.info("-------editingContainer == myRenderer-------");
+    	return myRenderer;
     }
 
     private boolean isRightToLeft() {
@@ -155,86 +184,152 @@ public class MyTreeCellEditor extends DefaultTreeCellEditor implements UIDepende
     public Component getTreeCellEditorComponent(JTree tree, Object value,
             boolean isSelected, boolean expanded, boolean leaf, int row) {
     	LOG.info("---------->JTree:"+tree + "\n value:"+value);
-    	return super.getTreeCellEditorComponent(tree, value, isSelected, expanded, leaf, row);
+//    	return super.getTreeCellEditorComponent(tree, value, isSelected, expanded, leaf, row);
+        setTree(tree);
+        lastRow = row;
+        determineOffset(tree, value, isSelected, expanded, leaf, row); // TODO auskommentieren?
+
+        if (editingComponent != null) {
+        	LOG.info("do not remove transient editingComponent");
+            //editingContainer.remove(editingComponent);
+        }
+        editingComponent = realEditor.getTreeCellEditorComponent(tree, value,
+                                        isSelected, expanded,leaf, row);
+    	if(editingComponent instanceof UoMCellEditor.UoMComponent uomComponent) {
+    		uomComponent.setEnabled(true);
+    	}
+    	// TODO uomComponent wird nur im editingComponent gezeigt, nicht im Tree renderer!!!
+
+        // this is kept for backwards compatibility but isn't really needed
+        // with the current BasicTreeUI implementation.
+        TreePath        newPath = tree.getPathForRow(row);
+
+        canEdit = (lastPath != null && newPath != null && lastPath.equals(newPath));
+
+        Font            font = getFont();
+
+        if(font == null) {
+            if(renderer != null)
+                font = renderer.getFont();
+            if(font == null)
+                font = tree.getFont();
+        }
+//        editingContainer.setFont(font);
+        prepareForEditing();
+        return editingContainer;
+    }
+    protected void prepareForEditing() {
+    	LOG.info("do nothing editingContainer:"+editingContainer);
+//        if (editingComponent != null) {
+//            editingContainer.add(editingComponent);
+//        }
+    	if (editingContainer == null) {
+    		editingContainer = createContainer();
+        	LOG.info(">>>> editingComponent:"+editingComponent);
+        	LOG.info(">>>> editingContainer:"+editingContainer);
+    	}
+    }
+
+    public boolean stopCellEditing() {
+    	LOG.info(">>>> realEditor:"+realEditor);
+        if(realEditor.stopCellEditing()) {
+            cleanupAfterEditing();
+            return true;
+        }
+        return false;
+    }
+    public void cancelCellEditing() {
+    	LOG.info(">>>> realEditor:"+realEditor);
+        realEditor.cancelCellEditing();
+        cleanupAfterEditing();
+    }
+    private void cleanupAfterEditing() {
+//        if (editingComponent != null) {
+//            editingContainer.remove(editingComponent);
+//        }
+        editingComponent = null;
     }
 
     public class XEditorContainer extends EditorContainer {
 
-        @Override
-        public Dimension getPreferredSize() {
-            if (isRightToLeft()) {
-                if(editingComponent != null) {
-                    Dimension         pSize = editingComponent.getPreferredSize();
-    
-                    pSize.width += offset + 5;
-    
-                    Dimension         rSize = (renderer != null) ?
-                                              renderer.getPreferredSize() : null;
-    
-                    if(rSize != null)
-                        pSize.height = Math.max(pSize.height, rSize.height);
-                    if(editingIcon != null)
-                        pSize.height = Math.max(pSize.height,
-                                                editingIcon.getIconHeight());
-    
-                    // trying to enforce a minimum size leads to field being painted over the icon
-                    // Make sure width is at least 100.
-    //                pSize.width = Math.max(pSize.width, 100);
-                    return pSize;
-                }
-                return new Dimension(0, 0);
-            }
-            return super.getPreferredSize();
-            
-        }
-
-        @Override
-        public void doLayout() {
-            if (isRightToLeft()) {
-                Dimension             cSize = getSize();
-
-                editingComponent.getPreferredSize();
-                editingComponent.setLocation(0, 0);
-                editingComponent.setBounds(0, 0,
-                                           cSize.width - offset,
-                                           cSize.height);
-            } else {
-
-                //super.doLayout();
-                if(editingComponent != null) {
-                    int width = getWidth();
-                    int height = getHeight();
-                    if (getComponentOrientation().isLeftToRight()) {
-                        editingComponent.setBounds(offset, 0, width - offset, height);
-                    } else {
-                        editingComponent.setBounds(0, 0, width - offset, height);
-                    }
-                }
-            }
-        }
-
-
-        @Override
-        public void paint(Graphics g) {
-            if (isRightToLeft()) {
-                Dimension size = getSize();
-
-                // Then the icon.
-                if (editingIcon != null) {
-                    int yLoc = Math.max(0, (size.height - editingIcon.getIconHeight()) / 2);
-                    int xLoc = Math.max(0, size.width - offset);
-                    editingIcon.paintIcon(this, g, xLoc, yLoc);
-                }
-                // need to prevent super from painting the icon
-                Icon rememberIcon = editingIcon;
-                editingIcon = null;
-                super.paint(g);
-                editingIcon = rememberIcon;
-                
-            } else {
-                super.paint(g);
-            }
-        }
+//        @Override
+//        public Dimension getPreferredSize() {
+//        	Dimension pSize = new Dimension(0, 0);
+//            if (isRightToLeft()) {
+//                if(editingComponent != null) {
+//                    pSize = editingComponent.getPreferredSize();
+//                	LOG.info("isRightToLeft : getPreferredSize="+pSize+" in editingComponent:"+editingComponent);
+//                    pSize.width += offset + 5;
+//    
+//                    Dimension         rSize = (renderer != null) ?
+//                                              renderer.getPreferredSize() : null;
+//    
+//                    if(rSize != null)
+//                        pSize.height = Math.max(pSize.height, rSize.height);
+//                    if(editingIcon != null)
+//                        pSize.height = Math.max(pSize.height,
+//                                                editingIcon.getIconHeight());
+//    
+//                    // trying to enforce a minimum size leads to field being painted over the icon
+//                    // Make sure width is at least 100.
+//    //                pSize.width = Math.max(pSize.width, 100);
+//                    return pSize;
+//                }
+//                return pSize;
+//            }
+//            pSize = super.getPreferredSize();
+//        	LOG.info("LeftToRight : PreferredSize="+pSize);
+//// PreferredSize=java.awt.Dimension[width=149,height=103] -- es ist der Containter im tree panel!!!
+//            return pSize;
+//        }
+//
+//        @Override
+//        public void doLayout() {
+//            if (isRightToLeft()) {
+//                Dimension             cSize = getSize();
+//
+//                editingComponent.getPreferredSize();
+//                editingComponent.setLocation(0, 0);
+//                editingComponent.setBounds(0, 0,
+//                                           cSize.width - offset,
+//                                           cSize.height);
+//            } else {
+//
+//                //super.doLayout();
+//                if(editingComponent != null) {
+//                    int width = getWidth();
+//                    int height = getHeight();
+//                    if (getComponentOrientation().isLeftToRight()) {
+//                        editingComponent.setBounds(offset, 0, width - offset, height);
+//                    } else {
+//                        editingComponent.setBounds(0, 0, width - offset, height);
+//                    }
+//                }
+//            }
+//        }
+//
+//
+//        @Override
+//        public void paint(Graphics g) {
+//            if (isRightToLeft()) {
+//                Dimension size = getSize();
+//
+//                // Then the icon.
+//                if (editingIcon != null) {
+//                    int yLoc = Math.max(0, (size.height - editingIcon.getIconHeight()) / 2);
+//                    int xLoc = Math.max(0, size.width - offset);
+//                    editingIcon.paintIcon(this, g, xLoc, yLoc);
+//                }
+//                // need to prevent super from painting the icon
+//                Icon rememberIcon = editingIcon;
+//                editingIcon = null;
+//                super.paint(g);
+//                editingIcon = rememberIcon;
+//                
+//            } else {
+//                super.paint(g);
+//            }
+//        }
         
     }
 
