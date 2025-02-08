11605cf1bdfd439099fa9143fd8fb673dc2cdce4
homebeaver <homebeaver@outlook.de>
2025-02-08 10:47:33 +0100

    TODO PropertyChangeListener

--- src/main/java/io/homebeaver/NodeElementContainer.java
+++ src/main/java/io/homebeaver/NodeElementContainer.java
@@ -6,7 +6,6 @@ import java.text.NumberFormat;
 import java.util.HashMap;
 
 import javax.swing.JCheckBox;
-import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -14,6 +13,7 @@ import javax.swing.border.TitledBorder;
 import javax.swing.tree.TreeNode;
 
 import org.jdesktop.swingx.JXFormattedTextField;
+import org.jdesktop.swingx.JXTextField;
 import org.json.simple.JSONObject;
 
 import io.homebeaver.uom.UoMTreeNode;
@@ -24,12 +24,24 @@ public class NodeElementContainer extends JPanel implements UoMTreeNodeContainer
 
 	static final String TITLE = "Node Element";
 	JCheckBox editSelected;
+	UoMTreeNode uomNode;
+//	DefaultListModel<UoMTreeNode> listModel; // TODO
 	public NodeElementContainer(JCheckBox editSelected) {
 		super(new GridLayout(0, 2));
         setBorder(new TitledBorder(TITLE));
         this.editSelected = editSelected;
 	}
 	
+	private JSONObject toJSONObject(UoMTreeNode uomNode) {
+    	if (uomNode instanceof UoMTreeNode.DirectoryTreeNode dtn) {
+    		JSONObject jsonUom = dtn.externalize(new JSONObject());
+    		return (JSONObject)jsonUom.get(UoMTreeNode.OBJECT);
+    	} else if (uomNode instanceof UoMTreeNode.QuantityTreeNode uom) { 
+    		JSONObject jsonUom = uom.externalize(new JSONObject());
+    		return (JSONObject)jsonUom.get(UoMTreeNode.OBJECT);
+    	}
+    	return null;
+	}
 	// in (super) public Component Container.add(Component comp)
     //            public Component Container.add(String name, Component comp) 
 	//            public Component Container.add(Component comp, int index)
@@ -39,15 +51,21 @@ public class NodeElementContainer extends JPanel implements UoMTreeNodeContainer
     	setBorder(new TitledBorder(parent == null ? TITLE : TITLE + " of Type "+uomNode.getParent()));
     	removeAll();
     	
-    	if (uomNode instanceof UoMTreeNode.DirectoryTreeNode dtn) {
-    		JSONObject jsonUom = dtn.externalize(new JSONObject());
-    		add((JSONObject)jsonUom.get(UoMTreeNode.OBJECT));
-    	} else if (uomNode instanceof UoMTreeNode.QuantityTreeNode uom) { 
-    		JSONObject jsonUom = uom.externalize(new JSONObject());
-    		add((JSONObject)jsonUom.get(UoMTreeNode.OBJECT));    		
-    	}
+    	add(toJSONObject(uomNode));
+    	
     	revalidate();
     	setVisible(true);
+//    	VetoableChangeListener ist uomNode; PropertyChangeEvent pce
+/* What is the difference between PropertyChangeListener and VetoableChangeListener?
+The main difference resides in the fact that 
+PropertyChangeListener are applied to bound properties while 
+VetoableChangeListener are applied to constrained properties.
+ */
+//    	VetoableChangeSupport vcs;
+//    	addVetoableChangeListener( pce -> {
+//    		System.out.println("PropertyChangeEvent "+pce);
+//    	});
+    	this.uomNode = uomNode;
     	return this;
 	}
 	
@@ -62,15 +80,17 @@ public class NodeElementContainer extends JPanel implements UoMTreeNodeContainer
     			JXFormattedTextField field = new JXFormattedTextField(format);
     			field.setValue(v);
     			field.setHorizontalAlignment(SwingConstants.RIGHT);
-    			field.setEditable(false); // read only
+//    			field.setEditable(false); // read only
     			label.setLabelFor(field);
     			add(label);
     			add(field);
        		} else if (v==null && "uomSymbol".equals(k)) {
        			// bei v==null uomSymbol nicht anzeigen
        		} else {
-    			JFormattedTextField field = new JFormattedTextField();
-    			field.setValue(v); // auch bei v==null
+//    			JXFormattedTextField field = new JXFormattedTextField();
+//    			field.setValue(v); // auch bei v==null
+    			JXTextField field = new JXTextField();
+    			field.setText((String)v); // auch bei v==null
     			label.setLabelFor(field);
     			add(label);
     			add(field);
@@ -89,8 +109,38 @@ public class NodeElementContainer extends JPanel implements UoMTreeNodeContainer
     	super.setEnabled(enabled);
 		Component[] components = getComponents();
 		for (Component c : components) {
-			if(c instanceof JFormattedTextField f) {
+			if(c instanceof JXFormattedTextField f) {
 				f.setEnabled(enabled);
+			} else if(c instanceof JXTextField f) {
+				f.setEnabled(enabled);
+				if(enabled) {
+					f.addPropertyChangeListener(pce -> {
+						Object source = pce.getSource();
+//			    		System.out.println("field PropertyChangeEvent "+pce
+//			    		+ "\n Source:"+source
+//			    		);
+			    		if(source instanceof JXTextField tf) {
+			    			JLabel labeledBy = (JLabel)tf.getClientProperty("labeledBy");
+//				    		System.out.println("field "+labeledBy.getText()+" value="+tf.getText());
+				    		HashMap<String, Object> jo = toJSONObject(uomNode);
+				    		if(tf.getText().equals(jo.get(labeledBy.getText()))) {
+				    			// unverändert
+				    		} else {
+					    		System.out.println("CHANGED field "+labeledBy.getText()+" value="+tf.getText());
+					    		// TODO das elem UoMTreeNode in listModel ermitteln und ändern
+				    		}
+			    		}
+/*
+        	LOG.info("amountXField PropertyChangeEvent:"+pce +
+        			"\n Source:"+pce.getSource());
+        	xamount = ((Number)amountXField.getValue()).doubleValue();
+        	Double d = Double.valueOf(computePayment(xamount, xrate, xnumPeriods));
+            paymentXField.setValue(d);
+            paymentXField.setForeground(d<0 ? Color.red : Color.black);
+
+ */
+					});
+				}
 			}
 		}
     }
