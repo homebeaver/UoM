1886ebf74eb48690a5045317d51d77db5968e1a8
homebeaver <homebeaver@outlook.de>
2025-02-18 14:46:50 +0100

    Quit with icon

---
+++ src/main/java/io/homebeaver/icon/KorellerRCircle_icons_power.java
@@ -0,0 +1,332 @@
+package io.homebeaver.icon;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.Stack;
+import javax.swing.plaf.UIResource;
+
+import org.jdesktop.swingx.icon.RadianceIcon;
+import org.jdesktop.swingx.icon.RadianceIconUIResource;
+
+/**
+ * This class has been automatically generated using 
+ * <a href="https://jdesktop.wordpress.com/2022/09/25/svg-icons/">Radiance SVG converter</a>.
+ */
+public class KorellerRCircle_icons_power implements RadianceIcon {
+    private Shape shape = null;
+    private GeneralPath generalPath = null;
+    private Paint paint = null;
+    private Stroke stroke = null;
+    private RadianceIcon.ColorFilter colorFilter = null;
+    private Stack<AffineTransform> transformsStack = new Stack<>();
+
+	// EUG https://github.com/homebeaver (rotation + point/axis reflection)
+    private int rsfx = 1, rsfy = 1;
+    public void setReflection(boolean horizontal, boolean vertical) {
+    	this.rsfx = vertical ? -1 : 1;
+    	this.rsfy = horizontal ? -1 : 1;
+    }    
+    public boolean isReflection() {
+		return rsfx==-1 || rsfy==-1;
+	}
+	
+    private double theta = 0;
+    public void setRotation(double theta) {
+    	this.theta = theta;
+    }    
+    public double getRotation() {
+		return theta;
+	}
+	// EUG -- END
+
+    
+
+	private void _paint0(Graphics2D g,float origAlpha) {
+transformsStack.push(g.getTransform());
+// 
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(0.015625f, 0.0f, 0.0f, 0.015625f, -0.0f, -0.0f));
+// _0
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_0
+shape = new Ellipse2D.Double(0.0, 0.0, 64.0, 64.0);
+paint = (colorFilter != null) ? colorFilter.filter(new Color(199, 92, 92, 255)) : new Color(199, 92, 92, 255);
+g.setPaint(paint);
+g.fill(shape);
+g.setTransform(transformsStack.pop());
+g.setComposite(AlphaComposite.getInstance(3, 0.2f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_1
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_1_0
+if (generalPath == null) {
+   generalPath = new GeneralPath();
+} else {
+   generalPath.reset();
+}
+generalPath.moveTo(32.0f, 52.0f);
+generalPath.curveTo(22.1f, 52.0f, 14.0f, 43.9f, 14.0f, 34.0f);
+generalPath.curveTo(14.0f, 27.6f, 17.4f, 21.7f, 22.9f, 18.5f);
+generalPath.curveTo(23.9f, 17.9f, 25.1f, 18.3f, 25.6f, 19.2f);
+generalPath.curveTo(26.2f, 20.2f, 25.800001f, 21.400002f, 24.9f, 21.900002f);
+generalPath.curveTo(20.7f, 24.4f, 18.0f, 29.1f, 18.0f, 34.0f);
+generalPath.curveTo(18.0f, 41.7f, 24.3f, 48.0f, 32.0f, 48.0f);
+generalPath.curveTo(39.7f, 48.0f, 46.0f, 41.7f, 46.0f, 34.0f);
+generalPath.curveTo(46.0f, 28.9f, 43.3f, 24.3f, 38.8f, 21.8f);
+generalPath.curveTo(37.8f, 21.3f, 37.5f, 20.0f, 38.0f, 19.099998f);
+generalPath.curveTo(38.5f, 18.099998f, 39.8f, 17.8f, 40.7f, 18.3f);
+generalPath.curveTo(46.5f, 21.5f, 50.0f, 27.5f, 50.0f, 34.0f);
+generalPath.curveTo(50.0f, 43.9f, 41.9f, 52.0f, 32.0f, 52.0f);
+generalPath.closePath();
+shape = generalPath;
+paint = (colorFilter != null) ? colorFilter.filter(new Color(35, 31, 32, 255)) : new Color(35, 31, 32, 255);
+g.setPaint(paint);
+g.fill(shape);
+g.setTransform(transformsStack.pop());
+g.setTransform(transformsStack.pop());
+g.setComposite(AlphaComposite.getInstance(3, 0.2f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_2
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_2_0
+if (generalPath == null) {
+   generalPath = new GeneralPath();
+} else {
+   generalPath.reset();
+}
+generalPath.moveTo(32.0f, 36.0f);
+generalPath.curveTo(30.9f, 36.0f, 30.0f, 35.1f, 30.0f, 34.0f);
+generalPath.lineTo(30.0f, 14.0f);
+generalPath.curveTo(30.0f, 12.9f, 30.9f, 12.0f, 32.0f, 12.0f);
+generalPath.curveTo(33.1f, 12.0f, 34.0f, 12.9f, 34.0f, 14.0f);
+generalPath.lineTo(34.0f, 34.0f);
+generalPath.curveTo(34.0f, 35.1f, 33.1f, 36.0f, 32.0f, 36.0f);
+generalPath.closePath();
+shape = generalPath;
+paint = (colorFilter != null) ? colorFilter.filter(new Color(35, 31, 32, 255)) : new Color(35, 31, 32, 255);
+g.setPaint(paint);
+g.fill(shape);
+g.setTransform(transformsStack.pop());
+g.setTransform(transformsStack.pop());
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_3
+paint = (colorFilter != null) ? colorFilter.filter(new Color(255, 255, 255, 255)) : new Color(255, 255, 255, 255);
+stroke = new BasicStroke(4.0f,1,0,10.0f,null,0.0f);
+if (generalPath == null) {
+   generalPath = new GeneralPath();
+} else {
+   generalPath.reset();
+}
+generalPath.moveTo(39.8f, 18.0f);
+generalPath.curveTo(44.7f, 20.7f, 48.0f, 26.0f, 48.0f, 32.0f);
+generalPath.curveTo(48.0f, 40.8f, 40.8f, 48.0f, 32.0f, 48.0f);
+generalPath.curveTo(23.2f, 48.0f, 16.0f, 40.8f, 16.0f, 32.0f);
+generalPath.curveTo(16.0f, 26.1f, 19.2f, 21.0f, 23.9f, 18.2f);
+shape = generalPath;
+g.setPaint(paint);
+g.setStroke(stroke);
+g.draw(shape);
+g.setTransform(transformsStack.pop());
+g.setComposite(AlphaComposite.getInstance(3, 1.0f * origAlpha));
+transformsStack.push(g.getTransform());
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_4
+paint = (colorFilter != null) ? colorFilter.filter(new Color(255, 255, 255, 255)) : new Color(255, 255, 255, 255);
+stroke = new BasicStroke(4.0f,1,0,10.0f,null,0.0f);
+shape = new Line2D.Float(32.000000f,32.000000f,32.000000f,12.000000f);
+g.setPaint(paint);
+g.setStroke(stroke);
+g.draw(shape);
+g.setTransform(transformsStack.pop());
+g.setTransform(transformsStack.pop());
+g.setTransform(transformsStack.pop());
+
+}
+
+
+
+	private void innerPaint(Graphics2D g) {
+        float origAlpha = 1.0f;
+        Composite origComposite = g.getComposite();
+        if (origComposite instanceof AlphaComposite) {
+            AlphaComposite origAlphaComposite = 
+                (AlphaComposite)origComposite;
+            if (origAlphaComposite.getRule() == AlphaComposite.SRC_OVER) {
+                origAlpha = origAlphaComposite.getAlpha();
+            }
+        }
+        
+	    _paint0(g, origAlpha);
+
+
+	    shape = null;
+	    generalPath = null;
+	    paint = null;
+	    stroke = null;
+        transformsStack.clear();
+	}
+
+    /**
+     * Returns the X of the bounding box of the original SVG image.
+     * 
+     * @return The X of the bounding box of the original SVG image.
+     */
+    public static double getOrigX() {
+        return 0.0;
+    }
+
+    /**
+     * Returns the Y of the bounding box of the original SVG image.
+     * 
+     * @return The Y of the bounding box of the original SVG image.
+     */
+    public static double getOrigY() {
+        return 0.0;
+    }
+
+	/**
+	 * Returns the width of the bounding box of the original SVG image.
+	 * 
+	 * @return The width of the bounding box of the original SVG image.
+	 */
+	public static double getOrigWidth() {
+		return 1.0;
+	}
+
+	/**
+	 * Returns the height of the bounding box of the original SVG image.
+	 * 
+	 * @return The height of the bounding box of the original SVG image.
+	 */
+	public static double getOrigHeight() {
+		return 1.0;
+	}
+
+	/** The current width of this icon. */
+	private int width;
+
+    /** The current height of this icon. */
+	private int height;
+
+	/**
+	 * Creates a new transcoded SVG image. This is marked as private to indicate that app
+	 * code should be using the {@link #of(int, int)} method to obtain a pre-configured instance.
+	 */
+	private KorellerRCircle_icons_power() {
+        this.width = (int) getOrigWidth();
+        this.height = (int) getOrigHeight();
+	}
+
+    @Override
+	public int getIconHeight() {
+		return height;
+	}
+
+    @Override
+	public int getIconWidth() {
+		return width;
+	}
+
+	@Override
+	public synchronized void setDimension(Dimension newDimension) {
+		this.width = newDimension.width;
+		this.height = newDimension.height;
+	}
+
+    @Override
+    public boolean supportsColorFilter() {
+        return true;
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        this.colorFilter = colorFilter;
+    }
+
+    @Override
+	public synchronized void paintIcon(Component c, Graphics g, int x, int y) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        if(getRotation()!=0) {
+            g2d.rotate(getRotation(), x+width/2, y+height/2);
+        }
+        if(isReflection()) {
+        	g2d.translate(x+width/2, y+height/2);
+        	g2d.scale(this.rsfx, this.rsfy);
+        	g2d.translate(-x-width/2, -y-height/2);
+        }
+		g2d.translate(x, y);
+
+        double coef1 = (double) this.width / getOrigWidth();
+        double coef2 = (double) this.height / getOrigHeight();
+        double coef = Math.min(coef1, coef2);
+        g2d.clipRect(0, 0, this.width, this.height);
+        g2d.scale(coef, coef);
+        g2d.translate(-getOrigX(), -getOrigY());
+        if (coef1 != coef2) {
+            if (coef1 < coef2) {
+               int extraDy = (int) ((getOrigWidth() - getOrigHeight()) / 2.0);
+               g2d.translate(0, extraDy);
+            } else {
+               int extraDx = (int) ((getOrigHeight() - getOrigWidth()) / 2.0);
+               g2d.translate(extraDx, 0);
+            }
+        }
+        Graphics2D g2ForInner = (Graphics2D) g2d.create();
+        innerPaint(g2ForInner);
+        g2ForInner.dispose();
+        g2d.dispose();
+	}
+    
+    /**
+     * Returns a new instance of this icon with specified dimensions.
+     *
+     * @param width Required width of the icon
+     * @param height Required height of the icon
+     * @return A new instance of this icon with specified dimensions.
+     */
+    public static RadianceIcon of(int width, int height) {
+       KorellerRCircle_icons_power base = new KorellerRCircle_icons_power();
+       base.width = width;
+       base.height = height;
+       return base;
+    }
+
+    /**
+     * Returns a new {@link UIResource} instance of this icon with specified dimensions.
+     *
+     * @param width Required width of the icon
+     * @param height Required height of the icon
+     * @return A new {@link UIResource} instance of this icon with specified dimensions.
+     */
+    public static RadianceIconUIResource uiResourceOf(int width, int height) {
+       KorellerRCircle_icons_power base = new KorellerRCircle_icons_power();
+       base.width = width;
+       base.height = height;
+       return new RadianceIconUIResource(base);
+    }
+
+    /**
+     * Returns a factory that returns instances of this icon on demand.
+     *
+     * @return Factory that returns instances of this icon on demand.
+     */
+    public static Factory factory() {
+        return KorellerRCircle_icons_power::new;
+    }
+}
+
